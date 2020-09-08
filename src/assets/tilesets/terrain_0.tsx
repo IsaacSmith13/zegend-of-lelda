afc039e1bc83c9e3ede24e018214045ec92c339a
IsaacSmith13 <Isaac13Smith@gmail.com>
2020-09-08 17:56:19 -0400

    Added tester level and tileset

---
+++ src/assets/tilesets/terrain_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Spritepack" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="../../../../totally-generic-platformer/com.isaactsmith.platformer/resources/tilesets/Spritepack.png" width="256" height="159"/>
+</tileset>
